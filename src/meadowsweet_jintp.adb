23bb2202ba713280f2d7df045ab948b2ffda64d3
Rene Hartmann <rehartmann@t-online.de>
2026-06-19 22:59:06 +0200

    Source code added

---
+++ src/meadowsweet_jintp.adb
@@ -0,0 +1,106 @@
+with Ada.Strings.Unbounded;
+with Servlet.Streams;
+with Util.Beans.Objects;
+with Jintp;
+
+package body Meadowsweet_Jintp is
+
+   use Ada.Strings.Unbounded;
+   use Util.Beans.Objects;
+
+   type Inspectable_Bean_Access is access all Meadowsweet.Inspectable_Bean;
+
+   procedure To_Dictionary (Source : Meadowsweet.Inspectable_Bean'Class;
+                            Target : in out Jintp.Dictionary);
+
+   procedure To_List (Source : Object;
+                      Target : in out Jintp.List) is
+      Length : constant Natural := Get_Count (Source);
+      Value : Object;
+   begin
+      for I in 1 .. Length loop
+         Value := Get_Value (Source, I);
+         case Get_Type (Value) is
+            --  currently not supported by JinTP
+            --  when TYPE_BOOLEAN =>
+            --      Target.Append (To_Boolean (Value));
+            --  when TYPE_INTEGER =>
+            --      Target.Append (To_Integer (Value));
+            --  when TYPE_FLOAT =>
+            --      Target.Append (To_Long_Float (Value));
+            when TYPE_BEAN =>
+               declare
+                  D : Jintp.Dictionary;
+               begin
+                  To_Dictionary (Inspectable_Bean_Access (To_Bean (Value)).all,
+                                 D);
+                  Target.Append (D);
+               end;
+            when TYPE_ARRAY =>
+               declare
+                  L : Jintp.List;
+               begin
+                  To_List (Value, L);
+                  Target.Append (L);
+               end;
+            when others =>
+               Target.Append (To_String (Value));
+         end case;
+      end loop;
+   end To_List;
+
+   procedure To_Dictionary (Source : Meadowsweet.Inspectable_Bean'Class;
+                            Target : in out Jintp.Dictionary) is
+      Property_Names : constant Meadowsweet.String_Array
+        := Source.Property_Names;
+      Value : Object;
+      Value_Type : Data_Type;
+   begin
+      for I in Property_Names'Range loop
+         Value := Source.Get_Value (To_String (Property_Names (I)));
+         Value_Type := Get_Type (Value);
+         case Value_Type is
+            when TYPE_BOOLEAN =>
+               Target.Insert (Property_Names (I), To_Boolean (Value));
+            when TYPE_INTEGER =>
+               Target.Insert (Property_Names (I), To_Integer (Value));
+            when TYPE_FLOAT =>
+               Target.Insert (Property_Names (I), To_Long_Float (Value));
+            when TYPE_BEAN =>
+               declare
+                  D : Jintp.Dictionary;
+               begin
+                  To_Dictionary (Inspectable_Bean_Access (To_Bean (Value)).all,
+                                 D);
+                  Target.Insert (Property_Names (I), D);
+               end;
+            when TYPE_ARRAY =>
+               declare
+                  L : Jintp.List;
+               begin
+                  To_List (Value, L);
+                  Target.Insert (Property_Names (I), L);
+               end;
+            when others =>
+               Target.Insert (Property_Names (I), To_String (Value));
+         end case;
+      end loop;
+   end To_Dictionary;
+
+   overriding procedure Render_Response
+     (This : Jintp_Renderer;
+      View : String;
+      Model : Meadowsweet.Inspectable_Bean'Class;
+      Response : in out Servlet.Responses.Response'Class)
+   is
+      Stream : Servlet.Streams.Print_Stream := Response.Get_Output_Stream;
+      Content : Unbounded_String;
+      Values : Jintp.Dictionary;
+   begin
+      To_Dictionary (Model, Values);
+      Content := Jintp.Render (View, Values);
+      Response.Set_Content_Length (Length (Content));
+      Stream.Write (Content);
+   end Render_Response;
+
+end Meadowsweet_Jintp;
---
+++ src/meadowsweet_jintp.ads
@@ -0,0 +1,14 @@
+with Meadowsweet;
+with Servlet.Responses;
+
+package Meadowsweet_Jintp is
+
+   type Jintp_Renderer is new Meadowsweet.Renderer with null record;
+
+   overriding procedure Render_Response
+     (This : Jintp_Renderer;
+      View : String;
+      Model : Meadowsweet.Inspectable_Bean'Class;
+      Response : in out Servlet.Responses.Response'Class);
+
+end Meadowsweet_Jintp;
